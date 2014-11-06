04576c3a33faf1594a7a3f79db66060da2a6325f
Marike Grinde <mg222cd@student.lnu.se>
2014-11-06 23:40:41 +0100

    kommentarer om tillvägagångssätt

--- index.php
+++ index.php
@@ -5,3 +5,15 @@ $view = new \View\HTMLView();
 $test = "här har jag skrivit text";
 
 $view->echoHTML($test);
+
+/*
+man kör alltså ex $dom->loadHTML($data)
+och datan är resultatet av curl
+
+lägga till header och followlocation:
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+*/
