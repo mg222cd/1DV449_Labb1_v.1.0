c3fd9046b64f168d6fe0d91e7743f308a5ba0cd2
Marike Grinde <mg222cd@student.lnu.se>
2014-11-07 12:00:55 +0100

    curl get hämtar hela sidan!

--- Controller/WebscraperController.php
+++ Controller/WebscraperController.php
@@ -10,6 +10,17 @@ class WebscraperController{
 	}
 
 	public function doControl(){
+		//curl get
 		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+
+		$data = curl_exec($ch);
+		curl_close($ch); // <-- inte viktigt men "god sed"
+
+		return $data;
 	}
 }
