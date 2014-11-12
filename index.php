cbbd804d9e6fc39fc5147340b81da40dd2ebe722
Marike Grinde <mg222cd@student.lnu.se>
2014-11-12 15:12:50 +0100

    småfix med json-filen

--- index.php
+++ index.php
@@ -5,15 +5,19 @@ require_once('View/JsonView.php');
 $controller = new \Controller\WebscraperController();
 $jsonView = new \View\JsonView();
 
-//ta bort sen
-//header('Content-type: application/json; charset=UTF-8');
-
 //main-functions.
 $file = 'file.json';
 $jsonView->setContentHeader('application/json;');
 
+//testcode to demonstrate new scrape:
+/*
+$data = $controller->doControl();
+file_put_contents($file, $data);
+echo $data;
+*/
+
 //validate it latest scrape is made less than 5 minutes ago.
-$data = file_get_contents('file.json');
+$data = file_get_contents($file);
 $dataDecoded = json_decode($data);
 $scrapedTime = $dataDecoded->latest_scrape;
 $cacheExpire = date('Y/m/d H:i:s', strtotime('- 5 minutes'));
