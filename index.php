c54a18789b734b761c637c9ff966e59aacf4ae04
Marike Grinde <mg222cd@student.lnu.se>
2014-11-12 14:53:16 +0100

    fungerande skrapa och cache!

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
-//require_once('View/HTMLView.php');
 require_once('Controller/WebscraperController.php');
 require_once('View/JsonView.php');
 
-//$view = new \View\HTMLView();
 $controller = new \Controller\WebscraperController();
 $jsonView = new \View\JsonView();
 
+//ta bort sen
+//header('Content-type: application/json; charset=UTF-8');
+
+//main-functions.
 $file = 'file.json';
+$jsonView->setContentHeader('application/json;');
 
-//$jsonView->setContentHeader('application/json;');
-header('Content-type: application/json; charset=UTF-8');
-//file_put_contents($file, $controller->doControl());
-echo $controller->doControl();
-
-//om tiden gått ut
-//...gör en ny skrapning
-//...och visa filen
-//Annars
-//...visa cache
+//validate it latest scrape is made less than 5 minutes ago.
+$data = file_get_contents('file.json');
+$dataDecoded = json_decode($data);
+$scrapedTime = $dataDecoded->latest_scrape;
+$cacheExpire = date('Y/m/d H:i:s', strtotime('- 5 minutes'));
+if ($scrapedTime >= $cacheExpire) {
+	//show cache
+	echo $data;
+} 
+else {
+	//new scrape
+	$data = $controller->doControl();
+	file_put_contents($file, $data);
+	echo $data;
+}
